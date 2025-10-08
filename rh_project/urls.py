a4a4bf700739471f2dd4d27f3f701f69a1baf486
el Alex <alejandro.mendez.gomez@hotmail.com>
2025-10-08 11:38:02 -0600

    login 2

--- rh_project/urls.py
+++ rh_project/urls.py
@@ -15,4 +15,5 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+    urlpatterns += staticfiles_urlpatterns()
